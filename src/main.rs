34a382d95ab4cbd906d87387b99db1eb31a0762f
Joseph McGovern <joseph.mcgovern@webfilings.com>
2019-05-21 10:31:03 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,25 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     let branch = format!("pull_{}", args.pull_number);
-    let branch_to_pull = format!("refs/pull/{}/head:{}", args.pull_number, branch);
+    let base_branch_to_pull = format!("pull/{}/head", args.pull_number);
+    let branch_to_pull = format!("{}:{}", base_branch_to_pull, branch);
     let branch_output = Command::new("git").arg("branch-current").output().expect("Error fetching current branch");
     if branch_output.status.success() {
         let current_branch = String::from_utf8(branch_output.stdout).expect("Expected stdout to be utf8 vector");
         if current_branch.trim() == branch {
-            println!("Already on branch {}", branch);
+            println!("Already on branch {}. Pulling most recent changes...", branch);
+            get_branch("fetch", &base_branch_to_pull);
+            get_branch("pull", &base_branch_to_pull);
             return;
         }
     }
-    let output = Command::new("git").arg("fetch").arg("upstream").arg(&branch_to_pull).output().expect("Error fetching branch");
+    get_branch("fetch", &branch_to_pull);
+    Command::new("git").arg("checkout").arg(branch).status().expect("Failed to fetch branch");
+}
+
+
+fn get_branch(action: &str, branch: &String) {
+    let output = Command::new("git").arg(action).arg("upstream").arg(&branch).output().expect("Error fetching branch");
     if output.status.success() {
         let stdout = String::from_utf8(output.stdout).expect("Expected stdout to be utf8 vector");
         if stdout.trim().len() > 0 {
@@ -29,10 +38,6 @@ fn main() {
         }
     }
     else {
-        let output = Command::new("git").arg("fetch").arg("origin").arg(&branch_to_pull).status().expect("Error fetching branch");
-        if !output.success() {
-            return;
-        }
+        Command::new("git").arg(action).arg("origin").arg(&branch).output().expect("Error fetching branch");
     }
-    Command::new("git").arg("checkout").arg(branch).status().expect("Failed to fetch branch");
 }
